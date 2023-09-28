0544912d5bf2d5ffb1fcfea2745ebf4ac3cac870
Bikramjeet singh <bs863313@gmail.com>
2023-09-28 07:08:44 +0530

    Props And Children topic Done

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,28 @@ import Test from "./Components/Test/Test";
 function App() {
   const [count, setCount] = useState(0);
 
+  let testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+  let testObj = {
+    name: "Jaskaran",
+  };
+
+  function testFunction() {
+    console.log("This function is inside App.js Component");
+  }
+
   return (
     <>
       <div>
-        Alert Massage Is
-        <Alert />
+        <Alert
+          message="This is from prop message"
+          dataArray={testArray}
+          dataObject={testObj}
+          testFun={testFunction}
+        >
+          <Test />
+        </Alert>
       </div>
-      <div>
-        This is Text File
-        <Test />
-      </div>
-      <h3>
-        I'm Bikramjeet Singh And This is my first React project where i have
-        Import Two componenets Alert Amd Test
-      </h3>
     </>
   );
 }
--- src/Components/Alert/Alert.jsx
+++ src/Components/Alert/Alert.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import "./Alert.css";
 
-export default function Alert() {
+export default function Alert(props) {
+  console.log(props);
+
+  // let dataArray = props.dataArray;
+  // let message = props.message;
+  // console.log(dataArray);
+
+  let { dataArray, dataObject, message, testFun } = props;
+  // console.log(dataArray, dataObject, message, testFun);
+
+  // testFun();
+  // console.log(props.dataArray);
+  // console.log(props.dataObject);
+  // console.log(props.testFun);
   return (
     <div className="alert">
-      <h1>This is Alert Jsx file Under Alert components </h1>
-      <h4>here i include alert.css file </h4>
+      <h1>{message}</h1>
+      {props.children}
     </div>
   );
 }
