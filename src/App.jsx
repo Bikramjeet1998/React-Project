0b6f361b0c3507973dddbde3bb62f13e433c4fe3
karanmalhii <karanmalhii1313@gmail.com>
2023-09-29 06:16:36 +0530

    basic practice

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "./App.css";
 import Message from './components/Message/Message';
 import Alert from "./components/Alert/Alert";
+import Mywork from "./practice/mywork/mywork";
 
 export default function App() {
   return (
@@ -9,6 +10,8 @@ export default function App() {
       <h1>This working 👌</h1>
       <Message/>
       <Alert></Alert>
+      <Mywork/>
+      
     </div>
   )
 }
---
+++ src/practice/Mywork/Mywork.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Mywork.css"
+
+export default function Mywork() {
+  let name = "karan malhi"
+  return (
+    <div>
+      <p>This is a work.jsx under mywork folder.</p>
+      <p>{name}</p>
+    </div>
+  )
+}
